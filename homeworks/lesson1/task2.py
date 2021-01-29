2f4cd91785f4af840ac067d57c1e99d47f971605
stolbovaya <stolbovaya@yahoo.com>
2021-01-29 16:29:18 +0300

    lesson1 task2

---
+++ homeworks/lesson1/task2.py
@@ -0,0 +1,32 @@
+"""2. Изучить список открытых API (https://www.programmableweb.com/category/all/apis).
+Найти среди них любое, требующее авторизацию (любого типа).
+Выполнить запросы к нему, пройдя авторизацию. Ответ сервера записать в файл."""
+import json
+import requests
+
+oauth_token = "AgAAAAAHdzZpAATuwcBgXrs4f0bfj-CcD2EIadA"
+
+
+def create_token(in_oauth_token) -> object:
+    f_params = {'yandexPassportOauthToken': in_oauth_token}
+    f_response = requests.post('https://iam.api.cloud.yandex.net/iam/v1/tokens', params=f_params)
+    decode_response = f_response.content.decode('UTF-8')
+    text = json.loads(decode_response)
+    iam_token = text.get('iamToken')
+
+    return iam_token
+
+
+IAM_oauth_token = create_token(oauth_token.format(oauth_token))
+
+print(f'Токен успешно сгенерирован {IAM_oauth_token} ')
+
+params = {'folder_id': 'b1gdcsv1e916eerpiba3',
+          'texts': ['Hello', 'World'], 'targetLanguageCode': 'ru'}
+
+headers = {'Authorization': 'Bearer ' + IAM_oauth_token}
+
+response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate', headers=headers,
+                         params=params)
+
+print(response.json())
